7de2580b2b909930931617b5ac583edeb38ace7a
Cedrick Tan <168982674+cecece-c@users.noreply.github.com>
2024-06-09 01:20:18 +0800

    Update keygen.py

--- keygen.py
+++ keygen.py
@@ -2,10 +2,10 @@
 from cryptography.fernet import Fernet
 
 
-# Generate master key and store value in 'master_key_1' (Bytes)
-master_key_1 = Fernet.generate_key()
+# Generate master key and store value in 'master_key' (Bytes)
+master_key = Fernet.generate_key()
 
 
-# Write master key to file 'masterkey1.key'
-with open("masterkey1.key", "wb") as filekey:
-    filekey.write(master_key_1)
+# Write master key to file 'masterkey.key'
+with open("masterkey.key", "wb") as filekey:
+    filekey.write(master_key)
