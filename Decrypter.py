066874990dd2deb7d1e1f0c1493813518a6cee39
Cedrick Tan <168982674+cecece-c@users.noreply.github.com>
2024-06-10 23:09:55 +0800

    Update Decrypter.py

--- Decrypter.py
+++ Decrypter.py
@@ -1,32 +1,40 @@
-# Import 'cryptography' and 'time' library
+# Import 'Fernet', 'os', 'sys' and 'time' libraries
 from cryptography.fernet import Fernet
-import time
+import os, sys, time
+
+
+# Decrypt file
+def decryption():
+    # Decrypt data using 'master_key' and store value in 'decrypted_data'. Write 'decrypted_data' to 'target'
+    with open(f"{current_working_directory}{target_file}", "wb") as target:
+        decrypted_data = use.decrypt(data)
+        target.write(decrypted_data)
+        print(f"\n'{target_file}' has been decrypted.")
+
+
+# Get current working directory of script and store value in 'current_working_directory' (String)
+current_working_directory = os.path.dirname(sys.argv[0])
+current_working_directory = current_working_directory.replace("c:", "") + "/"
 
 
 # Open target file as 'target' and read data. Store value in 'data'
 while True:
     try:
-        target_file = input("\nTarget file (relative path): ")
-        with open(target_file, "rb") as target:
+        target_file = input("\nTarget file: ")
+        with open(f"{current_working_directory}{target_file}", "rb") as target:
             data = target.read()
             break
     except FileNotFoundError:
         print("\nInvalid input. File not found.")
 
 
-# Open 'masterkey.key' as 'filekey' and store value in 'master_key'
-with open("masterkey.key", "rb") as filekey:
-    master_key = filekey.read()
-
-
-# Use 'master_key'
-use = Fernet(master_key)
-
-
-# Decrypt data using 'master_key' and store value in 'decrypted_data'. Write 'decrypted_data' to 'target'
-with open(target_file, "wb") as target:
-    decrypted_data = use.decrypt(data)
-    target.write(decrypted_data)
+# Get and use master key. Store value in 'master_key'
+try:
+    master_key = input("\nEnter master key: ")
+    use = Fernet(master_key)
+    decryption()
+except TypeError:
+    print("\nInvalid input.")
 
 
 # Exit program
