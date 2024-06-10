e6c39c788745f9268c71bbf1a1f517efeaaec24f
Cedrick Tan <168982674+cecece-c@users.noreply.github.com>
2024-06-10 23:09:19 +0800

    Update Keygen.py

--- Keygen.py
+++ Keygen.py
@@ -1,15 +1,22 @@
-# Import 'cryptography' and 'time' library
+# Import 'Fernet', 'os', 'sys' and 'time' libraries
 from cryptography.fernet import Fernet
-import time
+import os, sys, time
 
 
 # Generate master key and store value in 'master_key' (Bytes)
 master_key = Fernet.generate_key()
 
 
+# Get current working directory of script and store value in 'current_working_directory' (String)
+current_working_directory = os.path.dirname(sys.argv[0])
+current_working_directory = current_working_directory.replace("c:", "") + "/"
+
+
 # Write master key to file 'masterkey.key'
-with open("masterkey.key", "wb") as filekey:
+with open(f"{current_working_directory}masterkey.key", "wb") as filekey:
     filekey.write(master_key)
+    # Display message
+    print(f"\nMaster key has been generated and written to {current_working_directory}masterkey.key")
 
 
 # Exit program
